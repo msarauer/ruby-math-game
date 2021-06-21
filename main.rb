c19bcba2b1c0a31cf6f22fb008fb97990ed2b291
Mitchel Sarauer <mitchelsarauer@hotmail.com>
2021-06-21 13:24:41 -0600

    with this added, the functionality of the app is complete

--- main.rb
+++ main.rb
@@ -1,21 +1,28 @@
 require "./computer"
+require "./player"
 
-game = Computer.new
 
-game.random_numbers
+p1 = Player.new("Player 1")
+p2 = Player.new("Player 2")
+players = [p1, p2]
+game = Computer.new(players)
 
-puts game.question
-p game.solution
-p game.turn
+loop do
+  puts "#{game.current_player.name}: #{game.random_numbers}"
+  if game.compare(game.current_player.get_guess.to_i) 
+    puts "#{game.current_player.name}: YES! You are correct!"
 
+  else
+    puts "#{game.current_player.name}: Seriously? No!"
+    game.current_player.lives -= 1
+  end
+
+  break if game.current_player.lives < 1
+  puts "P1: #{p1.lives}/3 vs P2: #{p2.lives}/3"
+  game.next_turn
+  puts "----- NEW TURN -----"
+end
 game.next_turn
-
-p game. turn
-
-game.next_turn
-
-p game. turn
-
-game.next_turn
-
-p game. turn
+puts "#{game.current_player.name} wins with #{game.current_player.lives} lives remaining"
+puts "----- GAME OVER -----"
+puts "Good bye!"
--- player.rb
+++ player.rb
@@ -1,8 +1,16 @@
 # class player - initialize with lives, store their lives, get their answer
 class Player
   
-  def initialize(lives)
-    @lives = lives
-    @guess
+  attr_accessor :lives
+  attr_reader :guess, :name
+
+  def initialize(name)
+    @lives = 3
+    @guess = 0
+    @name = name
+  end
+
+  def get_guess
+    @guess = gets.chomp
   end
 end
