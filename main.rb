b9cae74fc2c798f5de0ce2ad344d565fa383a0a9
Mitchel Sarauer <mitchelsarauer@hotmail.com>
2021-06-21 12:07:19 -0600

    with this, the main file is created (currently only for testing computer class)

---
+++ main.rb
@@ -0,0 +1,21 @@
+require "./computer"
+
+game = Computer.new
+
+game.random_numbers
+
+puts game.question
+p game.solution
+p game.turn
+
+game.next_turn
+
+p game. turn
+
+game.next_turn
+
+p game. turn
+
+game.next_turn
+
+p game. turn
